39139b63803dbf226221b3f7a11fa8a99a14b19d
Jarek <jaroslaw.pela@wp.pl>
2023-11-08 23:08:53 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("          __        ___          \r\n __ __    )_)  )\\/)  )    __  __ \r\n    --   /__) (  ( _(_    --     \r\n                                 \r\n  _                              \r\n / ` _   ) _      ) _  _)_ _   _ \r\n(_. (_( ( (_ (_( ( (_( (_ (_) )  \r\n\n\n\n                                 ");
+
+
+
+
+
+
+            Console.WriteLine(  "dowolny tekst");
+
+
         }
     }
 }
