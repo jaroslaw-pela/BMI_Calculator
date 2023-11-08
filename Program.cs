e3dde68e9ffb95d9669dc48a92b9d406aace7f76
Jarek <jaroslaw.pela@wp.pl>
2023-11-08 23:03:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BMI_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
