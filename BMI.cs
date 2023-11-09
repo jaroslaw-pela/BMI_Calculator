f1d1e98b20365bbe27cb2c39e0ec1eed24e0df5b
Jarek <jaroslaw.pela@wp.pl>
2023-11-09 23:21:54 +0100

    added BMI class

---
+++ BMI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+    public class BMI
+    {
+
+        private double MassKg { get; set; }
+
+        private double Growth { get; set; }
+
+        public BMI(double massKg, double growth)
+        {
+            MassKg = massKg;
+            Growth = growth;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BMI_Calculator
+﻿using System;
+
+namespace BMI_Calculator
 {
     internal class Program
     {
@@ -6,10 +8,8 @@
         {
             Console.WriteLine("          __        ___          \r\n __ __    )_)  )\\/)  )    __  __ \r\n    --   /__) (  ( _(_    --     \r\n                                 \r\n  _                              \r\n / ` _   ) _      ) _  _)_ _   _ \r\n(_. (_( ( (_ (_( ( (_( (_ (_) )  \r\n\n\n\n                                 ");
 
-
-
-
-
+            double x, y = 0;
+            BMI bmi = new BMI(x, y);
 
             Console.WriteLine(  "dowolny tekst");
 
